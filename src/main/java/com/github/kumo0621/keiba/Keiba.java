ca2299ee23ab58e8a341b084f5098aa4ccb168da
kumo0621 <kumokumokumo0621@gmail.com>
2022-11-30 15:56:39 +0900

    掛け金に対応

--- src/main/java/com/github/kumo0621/keiba/Keiba.java
+++ src/main/java/com/github/kumo0621/keiba/Keiba.java
@@ -1,7 +1,9 @@
 package com.github.kumo0621.keiba;
 
+import jdk.internal.org.jline.reader.ParsedLine;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -9,8 +11,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public final class Keiba extends JavaPlugin implements org.bukkit.event.Listener {
 
@@ -30,6 +37,7 @@ public final class Keiba extends JavaPlugin implements org.bukkit.event.Listener
     }
 
     int number = 0;
+
     @EventHandler
     public void clickJoin(PlayerInteractAtEntityEvent event) {
         Player player = event.getPlayer();
@@ -48,17 +56,23 @@ public final class Keiba extends JavaPlugin implements org.bukkit.event.Listener
         if (command.getName().equals("kakeru")) {
             if (sender instanceof Player) {
                 Player player = ((Player) sender).getPlayer();
+                PlayerInventory inventory = Objects.requireNonNull(player).getInventory();
                 if (args.length <= 1) {
                     sender.sendMessage("/kakeru 名前 かけるダイヤの個数");
-                } else {
-                    config.set(String.valueOf(player), args[0]);
-                    config.set(String.valueOf(player), args[1]);
+                } else if (inventory.containsAtLeast(new ItemStack(Material.DIAMOND), Integer.parseInt(args[1]))) {
+                    config.set(player.getName(), args[0]);
+                    config.set(player.getName()+"setting", args[1]);
                     saveConfig();
-                    sender.sendMessage(args[0]+"さんに"+args[1]+"個かけました。");
-
+                    sender.sendMessage(args[0] + "さんに" + args[1] + "個かけました。");
+                } else {
+                    sender.sendMessage("掛け金が足りません");
+                    //sender.sendMessage("かけた人は"+(String) Objects.requireNonNull(config.get(player.getName())));
+                    //sender.sendMessage("かけた額は"+config.get(player.getName()+"setting"));
                 }
+
             }
         }
+
         if (command.getName().equals("start")) {
             if (sender instanceof Player) {
 
